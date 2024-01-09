36c411b3362cb10d2d7112c4ef86727a3a6414a5
fullstack-developer23 <ahmadsafeer1997gmail.com>
2024-01-09 11:15:55 +0000

    enhancement: add footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className='footer-wrapper'>
+            <div className='footer-inner-container'>
+            <h1 className='footer-title'>Awesome Footer</h1>
+            </div>
+        </div>
+      )
+}
+
+export default Footer
